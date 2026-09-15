873f61ab14dc6fd57f0fbea2f017f4ecda81be6c
Subhrakanti Bhattacharjee <subhra.k.bhatt88@gmail.com>
2026-09-15 17:45:00 +0530

    again

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AppRoutes from "./routes/appRoutes.jsx";
+
 import {Provider} from 'react-redux'
 import { store } from "./app/store.jsx";
 import { ToastContainer} from 'react-toastify';
@@ -12,6 +12,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AppRoutes from "./routes/AppRoutes.jsx";
 const queryClient = new QueryClient()
 createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
